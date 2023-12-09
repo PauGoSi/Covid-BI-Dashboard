ffe27543a8f8cb0214f67d35e035d66ef37946b6
Pau Go Si <Paugosi@hotmail.com>
2023-12-10 00:34:43 +0100

    Implement dynamic dropdown Province menu for each country

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 // Declare the DOM elements
-const countriesElement = document.getElementById("dropdown-countries")
-const mainElement = document.getElementById("covid-cases");
-
+const countriesElement = document.getElementById("dropdown-countries");
+const provincesElement = document.getElementById("dropdown-provinces");
+const covidcasesElement = document.getElementById("covid-cases");
 
 // Declare the API URL
 const apiCovidUrl = "https://disease.sh/v3/covid-19/historical/";
 
+// Define a global variable for storing fetched countries
+let countries;
+
 // Define a function for fetching the data from the API and returning it as a promise
-const getCovidApi = async (baseCovidApiUrl) => {
+const getCovidApi = async (baseCovidApiUrl, queryParameters = "") => {
     try {
-        const response = await fetch(baseCovidApiUrl);
+        const response = await fetch(`${baseCovidApiUrl}${queryParameters}`);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
@@ -19,103 +22,136 @@ const getCovidApi = async (baseCovidApiUrl) => {
         console.error("Error fetching data:", error);
         return null; // Return null in case of an error
     }
-}
+};
 
 // Define a function to create Highcharts chart
-const createChart = async (data) => {
+const createChart = (data, selectedProvince = null) => {
     try {
-        if (data && data.timeline && data.timeline.cases) {
-            Highcharts.chart(mainElement, {
-                title: {
-                    text: `COVID-19 Historical Data for ${data.country}`
-                },
-                xAxis: {
-                    categories: Object.keys(data.timeline.cases),
-                },
-                yAxis: {
-                    title: {
-                        text: 'Cases'
-                    }
-                },
-                series: [
-                    {
-                        name: 'Cases',
-                        data: Object.values(data.timeline.cases),
-                    },
-                    // Add more series for other data points (deaths, recovered, etc.)
-                ]
-            });
-        } else {
-            console.error("No valid data available to create chart.");
+        let titleText = `COVID-19 Historical Data for ${data.country}`;
+
+        if (selectedProvince) {
+            titleText += ` - ${selectedProvince}`;
         }
+
+        Highcharts.chart(covidcasesElement, {
+            title: {
+                text: titleText,
+            },
+            xAxis: {
+                categories: Object.keys(data.timeline.cases),
+            },
+            yAxis: {
+                title: {
+                    text: 'Cases',
+                },
+            },
+            series: [
+                {
+                    name: 'Cases',
+                    data: Object.values(data.timeline.cases),
+                },
+            ],
+        });
     } catch (error) {
         console.error("Error creating chart:", error);
     }
 };
 
-const getCountryData = async () => {
-    // Calling the fetchApi function
-    const countries = await getCovidApi(apiCovidUrl);
-
-    // Log the fetched countries to the console
-    console.log("Fetched countries:", countries);
-
-    // Looping through the array and creating an option for each country
-    for (const country of countries) {
-        const countryElement = document.createElement("option");
-        countryElement.value = country.country; // Use country name as the value
-        countryElement.appendChild(document.createTextNode(country.country + " " + country.province));
-        countriesElement.appendChild(countryElement);
+// Define a function to initialize the chart with default country on page load
+const initializeChart = async (countryName, selectedProvince = null) => {
+    if (!countries) {
+        console.error("Countries data is not available.");
+        return;
     }
 
-    // Set the default country
-    const defaultCountry = "Afghanistan";
+    const selectedCountry = countries.find(country => country.country === countryName);
 
-    // Initialize the chart with the default country on page load
-    const initializeChart = async (countryName) => {
-        const selectedCountry = countries.find(country => country.country === countryName);
+    if (selectedCountry) {
+        try {
+            // Fix the URL by removing the extra "historical"
+            let queryParameters = selectedProvince ? `/${selectedProvince}` : "";
+            const countryData = await getCovidApi(`${apiCovidUrl}${selectedCountry.country}`, queryParameters);
 
-        if (selectedCountry) {
-            try {
-                const countryData = await getCovidApi(`${apiCovidUrl}${selectedCountry.country}`);
-                
-                if (countryData && countryData.timeline) {
-                    createChart(countryData);
-                } else {
-                    console.error("No valid data available for the selected country.");
-                }
-            } catch (error) {
-                console.error("Error fetching data for the selected country:", error);
+            if (countryData && countryData.timeline) {
+                createChart(countryData, selectedProvince);
+            } else {
+                console.error("No valid data available for the selected country.");
             }
-        } else {
-            console.error("Selected country not found in the list.");
+        } catch (error) {
+            console.error("Error fetching data for the selected country:", error);
         }
-    };
+    } else {
+        console.error("Selected country not found in the list.");
+    }
+};
+// Define a function to get province data and populate the dropdown
+const getProvinceData = async (selectedCountry, selectedProvince = null) => {
+    provincesElement.innerHTML = ""; // Clear previous options
 
-    // Call initializeChart with the default country
-    initializeChart(defaultCountry);
+    // Fetch province data based on the selected country and province
+    let queryParameters = selectedProvince ? `/${selectedProvince}` : "";
+    const countryData = await getCovidApi(`${apiCovidUrl}${selectedCountry}`, queryParameters);
 
-    // Implementing the "change" event listener for the dropdown
-    // Displaying the Covid19 data of the country the user has selected in the dropdown menu
-    const handleCountryChange = async e => {
-        const selectedCountryName = e.target.value;
-        initializeChart(selectedCountryName);
-    };
+    if (countryData && countryData.timeline) {
+        console.log("Province Data:", countryData); // Log the fetched data
 
-    countriesElement.addEventListener('change', handleCountryChange);
+        // Populate the province dropdown
+        if (countryData.province) {
+            for (const province of countryData.province) {
+                const provinceElement = document.createElement("option");
+                provinceElement.value = province;
+                provinceElement.appendChild(document.createTextNode(province));
+                provincesElement.appendChild(provinceElement);
+            }
+        }
+
+        // Trigger the "change" event to update the chart when provinces are populated
+        provincesElement.dispatchEvent(new Event("change"));
+    } else {
+        console.error("No valid data available for the selected country.");
+    }
+};
+// Implementing the "change" event listener for the country dropdown
+// Displaying the Covid19 data of the country the user has selected in the dropdown menu
+const handleCountryChange = async e => {
+    const selectedCountryName = e.target.value;
+
+    // Call the function to populate the province dropdown based on the selected country
+    await getProvinceData(selectedCountryName);
+
+    const selectedProvince = provincesElement.value;
+    initializeChart(selectedCountryName, selectedProvince);
+};
+// Implementing the "change" event listener for the country dropdown
+// Displaying the Covid19 data of the country the user has selected in the dropdown menu
+countriesElement.addEventListener('change', handleCountryChange);
+
+// Implementing the "change" event listener for the province dropdown
+// Update the chart based on the selected province within the country
+const handleProvinceChange = async () => {
+    const selectedCountryName = countriesElement.value;
+    const selectedProvince = provincesElement.value;
+    initializeChart(selectedCountryName, selectedProvince);
 };
 
+// Implementing the "change" event listener for the province dropdown
+// Update the chart based on the selected province within the country
+provincesElement.addEventListener('change', handleProvinceChange);
+
 // Call getCountryData to initialize the dropdown and event listener
-getCountryData();
+countries = await getCovidApi(apiCovidUrl);
+console.log("Fetched countries:", countries);
 
+// Looping through the array and creating an option for each country
+for (const country of countries) {
+    const countryElement = document.createElement("option");
+    countryElement.value = country.country; // Use country name as the value
+    countryElement.appendChild(document.createTextNode(country.country));
+    countriesElement.appendChild(countryElement);
+}
 
+// Set the default country
+const defaultCountry = "Afghanistan";
 
-
-
-
-
-
-
-
-
-
+// Initialize the chart with the default country on page load
+initializeChart(defaultCountry);
